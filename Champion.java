e1d5fdc7bbd0abdcd9333355bb70ef5f66955ca6
Lailanater56 <chrislail@ymail.com>
2017-10-06 18:44:39 -0400

    Update Champion.java

--- Champion.java
+++ Champion.java
@@ -6,21 +6,21 @@
 	with masteries, runes, levels, and other stats.
  */
 public abstract class Champion
-{	
-   	protected String name;
-   	protected int level;
-   	protected double health;
-   	protected double healthRegen;
-   	protected double mana;
-   	protected double manaRegen;
-   	protected double attackDamage;
-   	protected double attackSpeed;
-   	protected double armor;
-   	protected double magicResist;
-   	protected boolean runes;
-   	public Champion(String name, double health,double healthRegen,double mana,
-					    double manaRegen,double attackDamage, double attackSpeed,
-					    double armor, double magicResist, int level)
+{
+    protected String name;
+    protected int level;
+    protected double health;
+    protected double healthRegen;
+    protected double mana;
+    protected double manaRegen;
+    protected double attackDamage;
+    protected double attackSpeed;
+    protected double armor;
+    protected double magicResist;
+    protected boolean runes;
+    public Champion(String name, double health,double healthRegen,double mana,
+		    double manaRegen,double attackDamage, double attackSpeed,
+		    double armor, double magicResist, int level)
     {
         this.name = name;
         this.level = level;
@@ -36,51 +36,52 @@ public abstract class Champion
     }
 
     /*
-		@args:	target - The champion you are attacking.
+        @args:	target - The champion you are attacking.
 
-		Calculates the damage per second you would do to your target.
-	 */
+        Calculates the damage per second you would do to your target.
+     */
     public double dps(Champion target)
-	{
+    {
 		return attackDamage * target.armorReduction() * attackSpeed;
-	}
+    }
 
-	// An abstract levelUp method
+    // An abstract levelUp method
     public abstract void levelUp();
+    
     /*
-		@args:		b - The base stat you are upgrading
-					g - The growth stat you are upgrading
-					n - The level of the champion
+	@args:		b - The base stat you are upgrading
+			g - The growth stat you are upgrading
+			n - The level of the champion
 
-		@return:	A double containing the new value of the upgraded stat
+	@return:	A double containing the new value of the upgraded stat
 
-		A formula used by Riot to calculate what a stat should be after a level up
-	 */
+	A formula used by Riot to calculate what a stat should be after a level up
+     */
     public double levelUpStat(double b, double g, int n)
-	{
-		return b + ((g * (n - 1)) * (.685 + (.0175 * n)));
-	}
-	/*
-		Calculates how much less damage you would take with the amount of armor you have
-	 */
-	public double armorReduction()
-	{
-		return 100 / (armor + 100); 
-	}
+    {
+        return b + ((g * (n - 1)) * (.685 + (.0175 * n)));
+    }
+    
+    /*
+        Calculates how much less damage you would take with the amount of armor you have
+     */
+    public double armorReduction()
+    {
+        return 100 / (armor + 100); 
+    }
 
-	/*
-		Calculates how much less damage you would take with the amount of magice resist you have
-	 */
+    /*
+        Calculates how much less damage you would take with the amount of magice resist you have
+     */
+    public double magicReduction()
+    {
+        return 100 / (magicResist + 100);
+    }
 
-	public double magicReduction()
-	{
-		return 100 / (magicResist + 100);
-	}
-
-	/*
-		Prints out all the champions current stats
-	 */
-	public void printChampionStats()
+    /*
+        Prints out all the champions current stats
+     */
+    public void printChampionStats()
     {
         System.out.println("Name: " + name);
         System.out.println("Level: " + level);
@@ -97,8 +98,8 @@ public abstract class Champion
     // TODO: THE RUNE METHODS BELOW NEED TO BE MOVED TO THEIR OWN CLASS
 
     /*
-		The stats added if you had a standard adc rune page
-	 */
+        The stats added if you had a standard adc rune page
+     */
     public void addStandardADCRunes()
     {
         attackDamage += 8.5;
