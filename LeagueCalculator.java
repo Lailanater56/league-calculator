f2abaaeb7e595aed4e3ac46f023ea61424f66e71
Chris Lail <chrislail56@gmail.com>
2017-10-06 17:43:44 -0400

    Added more comments to the LeagueCalculator.java class

--- LeagueCalculator.java
+++ LeagueCalculator.java
@@ -1,3 +1,10 @@
+/*
+	@author: Chris Lail
+	@version: 1.0
+
+	This class facilitates the main menu of the calculator and it's helper functions.
+ */
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -85,7 +92,11 @@ public class LeagueCalculator
 	
 	
 	// TODO: ERROR WON'T PRINT PRECISION. DOESN'T WORK
-	// TODO: Add descriptive comment
+	/*
+		Prints out whether the natural talent build had more lifesteal or the standard build had more
+		lifesteal. It also prints the difference of lifesteal between the two builds for each level
+		the user wanted to test.
+	 */
 	public void compareLifesteal()
 	{                                                                                                    
 		System.out.println("\n**********************Comparing Both Lifesteal**************************\n");
@@ -175,9 +186,9 @@ public class LeagueCalculator
 	
 	public static void main(String[] args)
 	{
-		LeagueCalculator test = new LeagueCalculator();
+		LeagueCalculator calc = new LeagueCalculator();
 		ChampionCreator cc = new ChampionCreator();
-		int choice = 1; //Set to one just to get into the while loop the first time
+		int choice = 0; //Set to 0 just to get into the while loop the first time
 		
 		int champIndex1;
 		int champIndex2;
@@ -187,88 +198,99 @@ public class LeagueCalculator
 		
 		while(choice != EXIT_CALCULATOR)
 		{
-			test.printMenu();
-			choice = test.input.nextInt();
+			calc.printMenu();
+			choice = calc.input.nextInt();
 			System.out.println();
 			switch(choice)
 			{
+				// Selected: Compare Lifesteal Between Natural Build (Korean) and Standard Build
 				case 1:
 					System.out.print("How many levels would you like to test? ");
-					test.levelsToTest = test.input.nextInt();
+					calc.levelsToTest = calc.input.nextInt();
 					System.out.println();
-					test.initNaturalTalentDamage();
-					test.initStandardDamage();
-					test.printNaturalTalentLifesteal();
-					test.printStandardLifeSteal();
+					calc.initNaturalTalentDamage();
+					calc.initStandardDamage();
+					calc.printNaturalTalentLifesteal();
+					calc.printStandardLifeSteal();
 					break;
-				
+
+				// Selected: Calculate DPS of One Champion On Another
 				case 2:
 					System.out.print("Enter the index of the champion that is attacking: ");
-					champIndex1 = test.input.nextInt();
-					firstChamp = test.champs.get(champIndex1);
+					champIndex1 = calc.input.nextInt();
+					firstChamp = calc.champs.get(champIndex1);
 					System.out.print("Enter the index of the champion that is taking damage: ");
-					champIndex2 = test.input.nextInt();
-					secondChamp = test.champs.get(champIndex2);
+					champIndex2 = calc.input.nextInt();
+					secondChamp = calc.champs.get(champIndex2);
 					System.out.printf("\n%s (%d) would have %f DPS against %s (%d)\n", firstChamp.name, champIndex1, firstChamp.dps(secondChamp),
 						secondChamp.name, champIndex2);
 					break;
-						
+
+				// Selected: Add New Champion
 				case 3:
 					System.out.print("Enter the name of a champion: ");
-					champName = test.input.next();
+					champName = calc.input.next();
 					System.out.println();
 					firstChamp = cc.findChamp(champName);
 					if (firstChamp == null)
 						System.out.println("That is not a valid champion.");
 					else
 					{
-						test.champs.add(firstChamp);
+						calc.champs.add(firstChamp);
 					}
 					break;
-					
+
+				// Selected: Delete a Champion By Index
 				case 4:
 					System.out.print("Enter the index of a champion that you would like to delete: ");
-					champIndex1 = test.input.nextInt();
-					test.champs.remove(champIndex1);
+					champIndex1 = calc.input.nextInt();
+					calc.champs.remove(champIndex1);
 					break;
-					
+
+				// Selected: Print List of Current Champions
 				case 5:
-					test.printAllChampions();
+					calc.printAllChampions();
 					break;
-					
+
+				// Selected: Add Runes To a Champion By Index
 				case 6:
 					System.out.print("Enter the index of a champion: ");
-					champIndex1 = test.input.nextInt();
+					champIndex1 = calc.input.nextInt();
 					System.out.println("Which Runes Would You Like To Add?");
 					System.out.println("1.   Standard");
 					System.out.println("2.   Arrow");
-					champIndex2 = test.input.nextInt(); //Not a champion, but a rune selection number
+					champIndex2 = calc.input.nextInt(); //Not a champion, but a rune selection number
 					if (champIndex2 == 1)
-						test.champs.get(champIndex1).addStandardADCRunes();
+						calc.champs.get(champIndex1).addStandardADCRunes();
 					else
-						test.champs.get(champIndex1).addArrowRunes();
+						calc.champs.get(champIndex1).addArrowRunes();
 					break;
-					
+
+				// Selected: Level Up a Champion By Index
 				case 7:
 					System.out.print("Enter the index of a champion: ");
-					champIndex1 = test.input.nextInt();
-					test.champs.get(champIndex1).levelUp();
+					champIndex1 = calc.input.nextInt();
+					calc.champs.get(champIndex1).levelUp();
 					break;
-					
+
+				// Selected: Print Champion Stats By Index
 				case 8:
 					System.out.print("Enter the index of a champion: ");
-					champIndex1 = test.input.nextInt();
-					test.champs.get(champIndex1).printChampionStats();
+					champIndex1 = calc.input.nextInt();
+					calc.champs.get(champIndex1).printChampionStats();
 					break;
-					
+
+				// Selected: Print All Champions Stats
 				case 9:
-					test.printAllChampStats();
+					calc.printAllChampStats();
 					break;
-					
+
+				// Selected: Exit The Menu
 				case EXIT_CALCULATOR:
 					System.out.println("Goodbye.");
 					break;
-					
+
+				// Selected: An invalid option for the calculator
 				default:
 					System.out.println("Sorry, that is an invalid choice. Please try again");
 			}
